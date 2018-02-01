41a9d35e5b0307fc44185beb2e109aa86b479c16
Felipe <felipejmesina@gmail.com>
2018-01-31 21:26:49 -0800

    removed cors

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
-const cors = require("cors");
 
-//Middleware
-app.use(cors ({
-  origin: "http://localhost:4200"
-}))
 
 app.use(express.static(path.join(__dirname + "/public/dist")));
 
